7ba75cb80587483eace3d341d1d3f3bb48dff55e
Daniel <wangyutang198@gmail.com>
2024-06-17 06:00:38 +0800

    建立創建表單頁面

---
+++ BulkyWeb3/Controllers/CategoryController.cs
@@ -0,0 +1,37 @@
+﻿using BulkyWeb3.Data;
+using BulkyWeb3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb3.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+            }
+                return RedirectToAction("Index");
+            }   
+        }
+    }
+
